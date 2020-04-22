3aa6e3b96de28de50ecc40a94d8e22d5cd458bfe
David Boedigheimer <stealth259@gmail.com>
2020-04-22 15:22:05 -0700

    Initial creation of top 10 games.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Template',
+      title: 'Top10Games',
       template: './src/index.html',
       inject: 'body'
     }),
