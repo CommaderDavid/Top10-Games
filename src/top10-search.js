a11d82199e4544f07dcfd94eaf5ed94aff5ca6cf
David Boedigheimer <stealth259@gmail.com>
2020-04-23 14:43:56 -0700

    Create a bestgames object for handling different consoles.

---
+++ src/top10-search.js
@@ -0,0 +1,24 @@
+export class BestGames {
+  constructor(pickedConsole) {
+    this.pickedConsole = pickedConsole;
+  }
+
+  top10Pick() {
+    // For the games
+    // axios({
+    //   url: "https://api-v3.igdb.com/games",
+    //   method: 'POST',
+    //   headers: {
+    //       'Accept': 'application/json',
+    //       'user-key': API_KEY
+    //   },
+    //   data: fields name,category,platforms,aggregated_rating; where category = 0 & platforms = this.pickedConsole & aggregated_rating > 85; sort aggregated_rating desc; limit 10;
+    // })
+    //   .then(response => {
+    //       console.log(response.data);
+    //   })
+    //   .catch(err => {
+    //       console.error(err);
+    //   });
+  }
+}
