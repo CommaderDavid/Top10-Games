a7248f7afc36008ce7adbb27cab4903132cd6212
David Boedigheimer <stealth259@gmail.com>
2020-05-27 13:59:23 -0700

    Add start of trial for getting api to work.

--- src/main.js
+++ src/main.js
@@ -3,24 +3,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { BestGames } from './top10-search.js';
+import axios from 'axios';
 
 $(document).ready(function () {
 
   $("form.consoles").submit(function (e) {
     e.preventDefault();
 
-    const userPicked = parsInt($("select#platform").val());
+    let consoleRequest = new BestGames
+    consoleRequest.top10Pick();
 
-    if (userPicked === 6) {
-      $("#user-pick").empty().append("PC");
-    } else if (userPicked === 48) {
-      $("#user-pick").empty().append("Playstation 4");
-    } else if (userPicked === 49) {
-      $("#user-pick").empty().append("XBox One");
-    } else if (userPicked === 130) {
-      $("#user-pick").empty().append("Nintendo Switch");
-    }
+    // const userPicked = parsInt($("select#platform").val());
+    //
+    // if (userPicked === 6) {
+    //   $("#user-pick").empty().append("PC");
+    // } else if (userPicked === 48) {
+    //   $("#user-pick").empty().append("Playstation 4");
+    // } else if (userPicked === 49) {
+    //   $("#user-pick").empty().append("XBox One");
+    // } else if (userPicked === 130) {
+    //   $("#user-pick").empty().append("Nintendo Switch");
+    // }
 
-    
   });
 });
--- src/top10-search.js
+++ src/top10-search.js
@@ -1,25 +1,24 @@
+import axios from 'axios';
 export class BestGames {
-  aysnc top10Pick(pickedConsole) {
-    try {
-      const response = await axios.get('/user')
-    } catch (error) {
-
-    }
+  top10Pick() {
     // For the games
-    // axios({
-    //   url: "https://api-v3.igdb.com/games",
-    //   method: 'POST',
-    //   headers: {
-    //       'Accept': 'application/json',
-    //       'user-key': API_KEY
-    //   },
-    //   data: fields name,category,platforms,aggregated_rating; where category = 0 & platforms = this.pickedConsole & aggregated_rating > 85; sort aggregated_rating desc; limit 10;
-    // })
-    //   .then(response => {
-    //       console.log(response.data);
-    //   })
-    //   .catch(err => {
-    //       console.error(err);
-    //   });
+    console.log('top10pick activated');
+    axios({
+      method: 'GET',
+      url: "https://api-v3.igdb.com/games",
+      crossDomain: true,
+      headers: {
+        'Accept': 'application/json',
+        'user-key': 
+
+      },
+      data: "fields name,category,platforms,aggregated_rating; where category = 0 & platforms = 48 & aggregated_rating > 85; sort aggregated_rating desc; limit 10;"
+    })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 }
